4a5c6da1e20b823b3dededece282e6f41f2f1c46
kal1van1ch <mishadolgopolov668@gmail.com>
2026-04-24 12:09:05 +0300

    Докинул endpoint с получением дерева

--- CampusHub.ConfigCenter/Endpoints/ConfigEndpoint.cs
+++ CampusHub.ConfigCenter/Endpoints/ConfigEndpoint.cs
@@ -1,5 +1,7 @@
 namespace CampusHub.ConfigCenter.Endpoints;
 
+using System.Text;
+
 
 public static class ConfigEndpoint {
     public static void MapConfigEndpoint(this WebApplication app) {
@@ -11,5 +13,31 @@ public static class ConfigEndpoint {
             Portal:Admin:Email {config["Portal:Admin:Email"]}
             """;
         });
+
+        app.Map("/config/section/portal", (IConfiguration config) => {
+            return config.GetSection("Portal");
+        });
+
+        app.Map("/config/tree", (IConfiguration config) => RecursiveMethod(config.GetSection("Portal")));
+    }
+
+    private static string RecursiveMethod(IConfiguration configSection) {
+
+        var sb = new StringBuilder();
+
+        foreach (var sec in configSection.GetChildren()) {
+            sb.Append(sec.Key);
+
+            if (sec.Value == null) {
+                sb.Append("\n");
+                var newSb = RecursiveMethod(sec);
+                sb.AppendLine(newSb);
+            }
+            else {
+                sb.Append($": {sec.Value}\n");
+            }
+        }
+
+        return sb.ToString();
     }
 }
